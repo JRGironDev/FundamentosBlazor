9c70edc37e77604aa6a9662aa314a1b7d4cf81b5
José Quiñonez <Jquinonez@tsc.com.gt>
2024-01-13 06:15:18 -0600

    Operatorias con código C#

---
+++ Pages/CodigoSegundoPlano.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FundamentosBlazor.Pages
+{
+	public partial class CodigoSegundoPlano
+	{
+        private bool mostrarMensaje = false;
+
+        private void MostrarMensaje()
+        {
+            mostrarMensaje = true;
+        }
+    }
+}
+
