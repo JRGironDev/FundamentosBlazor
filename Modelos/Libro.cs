a34bb7b88b295504bf8edebfd0144023e0f295de
José Quiñónez <joserobertoq@outlook.com>
2024-01-15 17:38:36 -0600

    Data Binding

---
+++ Modelos/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FundamentosBlazor.Modelos
+{
+	public class Libro
+	{
+		public int Id { get; set; }
+
+		public string Titulo { get; set; }
+
+        public bool Activo { get; set; }
+
+        public int Paginas { get; set; }
+
+    }
+}
+
